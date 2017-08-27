24abf7ebdc5527e0b3f7be772feb2f03385498e9
Mike Dreiling <mdarts@gmail.com>
2017-08-27 17:21:18 -0600

    Adding `build` to source control

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'mdarts.auth0.com',
   clientId: '9VB6T3secfEx4A4_hgfU1GE85co_nJ2N',
-  callbackUrl: 'foodies-plan-it.herokuapp.com/callback'
+  callbackUrl: 'http://foodies-plan-it.herokuapp.com/callback'
 }
